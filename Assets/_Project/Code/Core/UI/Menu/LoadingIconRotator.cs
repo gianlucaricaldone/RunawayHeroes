15eb45c9397b0d8072da8d1a63f266b8a1ceb7ab
Gianluca Ricaldone <gianlucaricaldone@gmail.com>
2025-04-28 00:30:38 +0200

    nuovo progetto ECS

--- Assets/_Project/Code/Core/UI/Menu/LoadingIconRotator.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class LoadingIconRotator : MonoBehaviour {
-    public float rotationSpeed = 90f; // Gradi al secondo
-    
-    void Update() {
-        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
-    }
-}
